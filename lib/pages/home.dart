037fcf8692c1b58775dc57a3c669399bffa88ef2
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-12-23 14:51:04 +0530

    Different fonts

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,15 +15,29 @@ class _HomeState extends State<Home> {
       body: Container(
           margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 10.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    "Hello New User!",
-                    style:AppWidget.boldTextFeildStyle()
-                  ),
+                  Text("Hello New User!",
+                      style: AppWidget.boldTextFeildStyle()),
+                  Container(
+                    padding: EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8)),
+                    child: Icon(Icons.shopping_cart, color: Colors.black),
+                  )
                 ],
-              )
+              ),
+              SizedBox(height: 20.0,),
+              Text(
+                "Delicious Food!",
+                style: AppWidget.HeadlineboldTextFeildStyle()
+                ),
+                Text("Discover and Get Greate Food!",
+                  style: AppWidget.lightboldTextFeildStyle()),
             ],
           )),
     );
--- lib/widgets/_widget_support.dart
+++ lib/widgets/_widget_support.dart
@@ -1,15 +1,27 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class AppWidget {
-
   static TextStyle boldTextFeildStyle() {
     return TextStyle(
         color: Colors.black,
         fontSize: 20.0,
         fontWeight: FontWeight.bold,
+        fontFamily: 'Poppins');
+  }
+
+  static TextStyle HeadlineboldTextFeildStyle() {
+    return TextStyle(
+        color: Colors.black,
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+        fontFamily: 'Poppins');
+  }
+  static TextStyle lightboldTextFeildStyle() {
+    return TextStyle(
+        color: Colors.black38,
+        fontSize: 15.0,
+        fontWeight: FontWeight.w500,
         fontFamily: 'Poppins');
   }
 }
