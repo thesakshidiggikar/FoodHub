3f14b20013c180552a2880b6abbac6924d37cc43
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2023-12-24 10:33:32 +0530

    Updated

---
+++ lib/pages/onboard.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Onboard extends StatefulWidget {
+  const Onboard({super.key});
+
+  @override
+  State<Onboard> createState() => _OnboardState();
+}
+
+class _OnboardState extends State<Onboard> {
+  int currentIndex = 0;
+  late PageController _controller;
+
+  @override
+  void initState() {
+    _controller = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        PageView.builder(
+          controller: _controller,
+          itemCount: ,
+          itemBuilder:(_, i) {
+          return Padding(padding: EdgeInsets.all(20),
+          child: Column(children: [],),)
+        },)
+      ],),
+    );
+  }
+}
